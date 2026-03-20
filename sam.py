dd005f199937ca8bc1a74d979f7116d509d05861
gobinathrit-tech <gobinathr2024it@jkkn.ac.in>
2026-03-20 12:16:39 +0530

    sample.py has been added

---
+++ sam.py
@@ -0,0 +1,12 @@
+"""for i in range(1,11):
+    print(i,"x2=",i*2)
+
+a=int(input())
+if(a%2==0):
+        print("even")
+"""
+a=[]
+for i in range(11):
+    num=int(input())
+    a.append(num)
+    print(a)
