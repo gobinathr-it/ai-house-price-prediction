bbda7476894362738f33028426b6ba176719288b
gobinathrit-tech <228479982+karthikeyanr2024it@users.noreply.github.com>
2026-03-25 19:56:01 +0530

    python practice

--- sam.py
+++ sam.py
@@ -1,12 +1,45 @@
-"""for i in range(1,11):
+"""
+for i in range(1,11):
     print(i,"x2=",i*2)
 
 a=int(input())
 if(a%2==0):
         print("even")
-"""
+
 a=[]
-for i in range(11):
-    num=int(input())
-    a.append(num)
-    print(a)
+print("enter 10 numbers:")
+for i in range(10):
+    num=int(input("enter num"+str(i+1)))
+    a.append(num)  
+print(a)
+
+for i in range(1,3):
+    print("week",i)
+    for j in range(1,4):
+        print("day:",j)
+
+for i in range(1,5):
+    print()
+    for j in range(1,i+1):
+        print("*",end="" )
+
+i=0
+while(i==0):
+    print(i)
+    i=i+1
+
+a = [1,2,3,4,5,6]
+a.append(True)
+a.append("gobi")
+a.append(5)
+
+a = [1,2,3,4,5,6]
+a[0]=11
+a.pop(3)
+print(a)
+
+"""
+a = [1,2,3,4,5,6,7]
+b = [7,8,9,0]
+a.extend(b)
+print(a)
